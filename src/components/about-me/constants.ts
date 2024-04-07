4c6b054992a033b75b927ae79c76bcb7689bb39b
BenMukebo <l.benkasmukebo7@gmail.com>
2024-04-07 17:41:21 +0200

    Add some languages, libraries and tools logo icon images and create a constant

---
+++ src/components/about-me/constants.ts
@@ -0,0 +1,45 @@
+interface technologyI {
+  name: string;
+  iconLogo: string;
+}
+
+export const languagesList: technologyI[] = [
+  { name: "JavaScript", iconLogo: "/icons/javascript-original.svg" },
+  { name: "Ruby", iconLogo: "/icons/ruby-original.svg" },
+  { name: "TypeScript", iconLogo: "/icons/typescript-original.svg" },
+  { name: "HTML", iconLogo: "/icons/html5-original.svg" },
+  { name: "CSS", iconLogo: "/icons/css3-original.svg" },
+  { name: "Arduino", iconLogo: "/icons/arduino-original-wordmark.svg" },
+  { name: "C++", iconLogo: "/icons/cplusplus-original.svg" },
+];
+
+export const frameworksLibraries: technologyI[] = [
+  { name: "Reactjs", iconLogo: "/icons/react-original-wordmark.svg" },
+  { name: "Ruby on rails", iconLogo: "/icons/rails-plain-wordmark.svg" },
+  { name: "Nextjs", iconLogo: "/icons/nextjs-original.svg" },
+  { name: "React Native", iconLogo: "/icons/react-original.svg" },
+  { name: "Redux", iconLogo: "/icons/redux-original.svg" },
+  { name: "React Query", iconLogo: "/icons/" },
+  { name: "Sass", iconLogo: "/icons/sass-original.svg" },
+  { name: "Tailwindcss", iconLogo: "/icons/tailwindcss-original.svg" },
+  { name: "PostgreSQL", iconLogo: "/icons/postgresql-original-wordmark.svg.svg" },
+  { name: "Supabase", iconLogo: "/icons/supabase-original.svg" },
+  { name: "Rswag", iconLogo: "/icons/swagger-original.svg" },
+  { name: "MySQL,", iconLogo: "/icons/mysql-original.svg" },
+  { name: "MaterialUI", iconLogo: "/icons/materialui-original.svg" },
+  { name: "Shadcn", iconLogo: "/icons/" },
+];
+
+export const toolsList: technologyI[] = [
+  { name: "Git", iconLogo: "/icons/git-original.svg" },
+  { name: "GitHub", iconLogo: "/icons/github-original.svg" },
+  { name: "Docker", iconLogo: "/icons/docker-original.svg" },
+  { name: "Figma", iconLogo: "/icons/figma-original.svg" },
+  { name: "Postman", iconLogo: "/icons/postgresql-plain.svg" },
+  { name: "Heroku", iconLogo: "/icons/heroku-original.svg" },
+  { name: "TDD", iconLogo: "/icons/" },
+  { name: "React testing", iconLogo: "/icons/" },
+  { name: "Jest", iconLogo: "/icons/" },
+  { name: "RSpec", iconLogo: "/icons/" },
+  { name: "Capybara,", iconLogo: "/icons/" },
+];
