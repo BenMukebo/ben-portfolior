e1f8c54438a6eeded505e182d63c4b064541c50c
BenMukebo <l.benkasmukebo7@gmail.com>
2024-04-21 11:20:04 +0200

    Add Slider in the About me section

--- src/components/about-me/AboutMe.tsx
+++ src/components/about-me/AboutMe.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Slider } from "@/components/ui/slider"
+
 import SlideTechSkills from "./slide-tech-skills/SlideTechSkills";
-import { frameworksLibraries, languagesList, toolsList } from "./constants";
+import { frameworksLibraries, greatExperience, languagesList, toolsList } from "./constants";
 
 // import { EmblaOptionsType } from "embla-carousel";
 
@@ -13,33 +15,61 @@ import { frameworksLibraries, languagesList, toolsList } from "./constants";
 export const AboutMe = () => {
 
   return (
-    <div className="flex__column__center justify-center gap-4 xl:flex-row">
-      <div className="xl:w-5/12 shrink-0">
+    <div className="flex__column__center gap-8  xl:flex-row xl:justify-between xl:items-stretch">
+      <div className="w-full max-w-3xl xl:w-[45%] grow-0">
         <h3 className="text-40 font-medium tracking-common text-white-foreground mb-4">
-          My Mission to develop
+          {/* My Mission to develop */}
+          Great Experience
         </h3>
+        <p className="text-white-light text-lg mb-6">
+          I have serious passion for UI effects,
+          animations and creating intuitive, dynamic user experiences.
+        </p>
+
+        <ul className="w-full flex__column gap-7">
+          {greatExperience.map((expereince, index) => (
+            <li key={index} className="">
+              <div className="flex__between text-white-foreground mb-3">
+                <h5 className="text-lg">
+                  {expereince.name}</h5>
+                <p className="text-base">
+                  {expereince.percentage}%</p>
+              </div>
+              <div>
+                <Slider
+                  defaultValue={[expereince.percentage]}
+                  max={100}
+                  step={1}
+                  disabled={true}
+                  isSliderThumb={true}
+                // className={cn("w-[60%]", expereince.percentage === 0 && "hidden")}
+                />
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
 
-      <div className="w-full max-w-4xl xl:max-w-2xl">
-        <article className="mb-6">
-          <h4 className="text-xl text-white-foreground tracking-common mb-4">
+      <div className="w-full max-w-3xl xl:w-5/12 grow-0 xl:max-w-2xl">
+        <article className="mb-5">
+          <h4 className="text-[20px] font-medium text-white-foreground tracking-common mb-2 px-2">
             Languages
           </h4>
 
           <SlideTechSkills skills={languagesList} />
         </article>
 
-        <article className="mb-6">
-          <h4 className="text-xl text-white-foreground tracking-common mb-4">
-            Labraries && Frameworks
+        <article className="mb-5">
+          <h4 className="text-[20px] font-medium text-white-foreground tracking-common mb-2 px-2">
+            Labraries & <span className="text-brand-foreground">Frameworks</span>
           </h4>
 
           <SlideTechSkills skills={frameworksLibraries} />
         </article>
 
-        <article className="mb-6">
-          <h4 className="text-xl text-white-foreground tracking-common mb-4">
-            Tools
+        <article className="mb-5">
+          <h4 className="text-[20px] font-medium text-white-foreground tracking-common mb-2 px-2">
+            Development Tools
           </h4>
 
           <SlideTechSkills skills={toolsList} />
