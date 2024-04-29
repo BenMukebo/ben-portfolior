88b2e75d359fdc22b6aa44f3d6d1dbe898a904ca
BenMukebo <l.benkasmukebo7@gmail.com>
2024-04-29 08:26:08 +0200

    Add loading, not-found pages and opengraph-image

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className="pt-12 pb-8 px-4 md:px-8 mx-auto ">
+      <h2 className="header__h1 pb-6 text-white-foreground text-center">
+        404 - Page Not Found
+      </h2>
+    </div>
+  )
+}
+
+export default NotFound;
